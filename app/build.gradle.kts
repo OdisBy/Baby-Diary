de68af99e31bef63ac20b8fe084c00faa10299ab
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-08-24 17:56:47 -0300

    Settings dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.io.realm)
+//    alias(libs.plugins.gms)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.ktlint)
 }
 
 android {
@@ -31,11 +38,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -50,8 +57,23 @@ android {
     }
 }
 
-dependencies {
+ktlint {
+    android = true
+    ignoreFailures = false
+    reporters {
+        reporter(ReporterType.PLAIN)
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.SARIF)
+    }
+    filter {
+        exclude("**/style-violations.kt")
+        exclude("**/generated/**")
+    }
+}
 
+
+
+dependencies {
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
@@ -67,4 +89,35 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage)
+
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.io.realm.library.sync)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.accompanist.pager)
+
+    implementation(libs.sheets.compose.dialogs.core)
+    implementation(libs.sheets.compose.dialogs.calendar)
+    implementation(libs.sheets.compose.dialogs.clock)
+
+    implementation(libs.messagebar.compose)
+    implementation(libs.one.tap.compose)
+    coreLibraryDesugaring(libs.desugar.jdk)
 }
